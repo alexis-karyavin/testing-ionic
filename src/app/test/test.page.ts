3450fea6cd2ce54dcdfdb3326d1a2c9fccb3d76e
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-04-13 14:08:36 +0300

    Added test component

---
+++ src/app/test/test.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.page.html',
+  styleUrls: ['./test.page.scss'],
+})
+export class TestPage implements OnInit {
+
+  public data;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
